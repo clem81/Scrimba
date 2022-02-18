0c9804e7d988fe26b525c030291c5af6d440e2c7
Matt <1945493+clem81@users.noreply.github.com>
2022-02-18 21:04:06 +0000

    finished passenger counter app

--- Passenger_Counter_Program/index.js
+++ Passenger_Counter_Program/index.js
@@ -1,12 +1,16 @@
-//document.getElementById("count-el").innerText = 5;
 let countEl = document.getElementById("count-el");
+let saveEl = document.getElementById("save-el");
 let count = 0;
 function increment() {
   //console.log("The button was clicked");
-  count = count + 1;
+  count += 1;
   countEl.innerText = count;
 }
 
 function save() {
-  console.log(count);
+  let message = count + " - ";
+  //saveEl.innerText += message; //innerText won't render the spaces properly which is why textContent is a better alternative
+  saveEl.textContent += message;
+  count = 0;
+  countEl.textContent = 0;
 }
