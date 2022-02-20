612ffaa1932271d0421071c7fc71c3549f4e1926
Matt <1945493+clem81@users.noreply.github.com>
2022-02-20 16:13:11 +0000

    unit conversion now uses user entered value

--- Unit_Conversion/index.js
+++ Unit_Conversion/index.js
@@ -1,42 +1,56 @@
-let convertValue = document.getElementById("convert-el");
 let length = document.getElementById("length-el");
 let volume = document.getElementById("volume-el");
 let mass = document.getElementById("mass-el");
-
-convertValue.textContent = 2000;
-let toConvert = convertValue.textContent;
 
 //Rounds to 3 dp
 function rounding(num) {
   return Math.round((num + Number.EPSILON) * 1000) / 1000;
 }
 
-function convertLength() {
-  let metres = rounding(toConvert / 3.28084);
-  let feet = rounding(toConvert * 3.28084);
+function convert() {
+  let toConvert = document.getElementById("convert-el").value;
+  convertLength(toConvert);
+  convertVolume(toConvert);
+  convertMass(toConvert);
+}
+
+function convertLength(toConvert) {
+  let toMetres = rounding(toConvert / 3.28084);
+  let toFeet = rounding(toConvert * 3.28084);
   length.textContent =
     toConvert +
     " metres = " +
-    feet +
+    toFeet +
     " feet | " +
     toConvert +
     " feet = " +
-    metres;
+    toMetres;
 }
 
-function convertVolume() {
-  let gallons = rounding(toConvert / 4.546);
-  let litres = rounding(toConvert * 4.546);
+function convertVolume(toConvert) {
+  let toGallons = rounding(toConvert / 4.546);
+  let toLitres = rounding(toConvert * 4.546);
   volume.textContent =
     toConvert +
     " gallons = " +
-    litres +
+    toLitres +
     " litres | " +
     toConvert +
     " litres = " +
-    gallons +
+    toGallons +
     " gallons";
 }
 
-convertLength();
-convertVolume();
+function convertMass(toConvert) {
+  let toPounds = rounding(toConvert * 2.20462);
+  let toKilos = rounding(toConvert / 2.20462);
+  mass.textContent =
+    toConvert +
+    "kilos = " +
+    toPounds +
+    "pounds | " +
+    toConvert +
+    "pounds = " +
+    toKilos +
+    "kilos";
+}
